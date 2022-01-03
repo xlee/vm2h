5eb4b7570510ad98e01dca136dc3aa611204ce1b
soaring eagle <xlee2004@gmail.com>
2022-01-04 00:10:17 +0800

    ok

--- start.sh
+++ start.sh
@@ -1,3 +1,30 @@
 #!/bin/sh
 
+cat <<EOF > /config.json 
+{
+    "log": {
+        "loglevel": "none"
+    },
+    "inbounds": [{
+            "port": "$PORT",
+            "protocol": "vmess",
+            "settings": {
+                "clients": [{
+                    "id": "$UUID"
+                }]
+            },
+            "streamSettings": {
+                "network": "ws",
+                "wsSettings": {
+                    "path": "/app"
+                }
+            }
+        }
+    ],
+    "outbounds": [{
+        "protocol": "freedom"
+    }]
+}
+EOF
+
 /usr/bin/v2ray -config /config.json
